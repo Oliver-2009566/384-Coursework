ae3c4e39eb7f1dc7e90a6f548e3694b9b8a1f697
Oolie <117578416+Oliver-2009566@users.noreply.github.com>
2023-03-01 16:25:17 +0000

    Added screen wrapping for the player

--- Assets/PlayerControls.cs
+++ Assets/PlayerControls.cs
@@ -18,6 +18,11 @@ public class PlayerControls : MonoBehaviour
         float inputX = Input.GetAxis("Horizontal");
         float inputY = Input.GetAxis("Vertical");
 
+        float topBoundary    =  5.5f;
+        float bottomBoundary = -5.5f;
+        float leftBoundary   = -10.65f;
+        float rightBoundary  =  10.65f;
+
         Vector3 movement = new Vector3(speed.x * inputX, speed.y * inputY, 0); 
         
         movement *= Time.deltaTime;
@@ -29,5 +34,18 @@ public class PlayerControls : MonoBehaviour
         Vector2 direction = mousePosition - transform.position;
         float angle = Vector2.SignedAngle(Vector2.right, direction);
         transform.eulerAngles = new Vector3(0, 0, angle);
+
+        if (transform.position.y > topBoundary) {
+            transform.position = new Vector3(transform.position.x, bottomBoundary, 0);
+        } else if (transform.position.y < bottomBoundary) {
+            transform.position = new Vector3(transform.position.x, topBoundary, 0);
+        }
+
+        if (transform.position.x < leftBoundary) {
+            transform.position = new Vector3(rightBoundary, transform.position.y, 0);
+        } else if (transform.position.x > rightBoundary) {
+            transform.position = new Vector3(leftBoundary, transform.position.y, 0);
+        }
+
     }
 }
